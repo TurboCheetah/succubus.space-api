2a9be381a67c6732b0d4d2fcbe21ccca8b73e5ab
TurboCheetah <me@turbo.ooo>
2020-07-28 18:40:41 -0500

    Ass implementation of a DB for testing purposes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('./config.json')
+const fakedb = require('./fakedb.json')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -7,6 +8,26 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render(`${__dirname}/views/index.ejs`)
+})
+
+app.get('/:hentaiID', (req, res) => {
+    let id = req.params.hentaiID;
+    if (fakedb.hentai[id]) {
+        res.render(`${__dirname}/views/hentai.ejs`, {hentai: fakedb.hentai[id]})
+        console.log(`Hentai "${fakedb.hentai[id].id}" accessed`) 
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+app.get('/api/:hentaiID', (req, res) => {
+    let id = req.params.hentaiID;
+    if (fakedb.hentai[id]) {
+        res.send(fakedb.hentai[id])
+        console.log(`Hentai "${fakedb.hentai[id].id}" accessed`) 
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 app.listen(process.env.PORT || config.port, () => {
