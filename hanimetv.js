e5da4054bec18f1d40b66e5772987e2e73bfe630
TurboCheetah <me@turbo.ooo>
2020-07-29 00:33:04 -0500

    Temporary(?) fix to get poster

--- hanimetv.js
+++ hanimetv.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 const scrape = async (query) => {
   if (!query.length) return "Baka! You didn't provide a search query! What am I supposed to search for?"
@@ -6,7 +7,17 @@ const scrape = async (query) => {
   const results = await fetch(`https://members.hanime.tv/rapi/v7/video?id=${query}`)
   .then((r) => r.json());
 
+  //Temporary(?) fix to get poster
+  const $ = await fetch(`https://hanime.tv/videos/hentai/${query}`)
+  .then((res) => {
+    if(!res.ok) throw "Something went wrong.";
+    return res.text();
+  })
+  .then((html) => cheerio.load(html));
+  
+  const poster = $('.hvpi-cover-container').find('img').attr('src')
   const video = results.hentai_video;
+  video.poster_url = poster;
   return video;
 };
 
