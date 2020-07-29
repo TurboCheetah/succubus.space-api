ccdbecdf759bd7e9082c0b540bdddefb98bdabc7
TurboCheetah <me@turbo.ooo>
2020-07-28 23:36:06 -0500

    Add HAnime.tv searching

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('./config.json')
 const fakedb = require('./fakedb.json')
+const hanime = require('./hanimetv')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -10,24 +11,38 @@ app.get('/', (req, res) => {
     res.render(`${__dirname}/views/index.ejs`)
 })
 
+//Sends back a nice parsed page
 app.get('/:hentaiID', (req, res) => {
     let id = req.params.hentaiID;
     if (fakedb.hentai[id]) {
         res.render(`${__dirname}/views/hentai.ejs`, {hentai: fakedb.hentai[id]})
-        console.log(`Hentai "${fakedb.hentai[id].id}" accessed`) 
     } else {
         res.sendStatus(404)
     }
 })
 
+//Sends back raw JSON
 app.get('/api/:hentaiID', (req, res) => {
     let id = req.params.hentaiID;
     if (fakedb.hentai[id]) {
         res.send(fakedb.hentai[id])
-        console.log(`Hentai "${fakedb.hentai[id].id}" accessed`) 
     } else {
         res.sendStatus(404)
     }
+})
+
+//Sends back raw JSON response from HAnime.tv API
+app.get('/api/hanime/:query', async (req, res) => {
+    let query = req.params.query;
+    const search = await hanime.scrape(query);
+    res.send(search)
+})
+
+//Sends back a nice parsed page
+app.get('/hanime/:query', async (req, res) => {
+    let query = req.params.query;
+    const search = await hanime.scrape(query);
+    res.render(`${__dirname}/views/hanime.ejs`, {hanime: search})
 })
 
 app.listen(process.env.PORT || config.port, () => {
