4303bd3e005c86c04c3789ac9f95a29006908118
Turbo <me@turbo.ooo>
2021-08-01 13:30:11 -0500

    feat: use modulus

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -2,7 +2,7 @@ import { ioRedis, client } from '../lib/redis.js'
 
 export const cache = async (req, res, next) => {
   // Fetch data from cache
-  if (req.path === '/random') req.params.query = Math.floor(Math.random() * parseInt(await ioRedis.get('newestID'))) + 1
+  if (req.path === '/random') req.params.query = Math.floor(Math.random() * +(await ioRedis.get('newestID'))) + 1
 
   const data = await client.get(req.params.query).catch(err => {
     console.error(err)
