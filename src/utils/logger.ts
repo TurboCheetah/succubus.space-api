555abf4e6579446ebe8ebfbdf17302f7653a3ed8
Turbo <me@turbo.ooo>
2021-09-05 19:28:35 -0500

    dsitroless

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,4 @@
-import { log } from '@/config'
-import fs from 'fs'
-import path from 'path'
 import winston from 'winston'
-import WinstonDaily from 'winston-daily-rotate-file'
-
-// logs dir
-const logDir: string = path.join(process.cwd(), log.dir)
-
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir)
-}
 
 // Define log format
 const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
@@ -26,35 +15,11 @@ const logger = winston.createLogger({
     logFormat
   ),
   transports: [
-    // debug log setting
-    new WinstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true
-    }),
-    // error log setting
-    new WinstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.splat(), winston.format.colorize())
     })
   ]
 })
-
-logger.add(
-  new winston.transports.Console({
-    format: winston.format.combine(winston.format.splat(), winston.format.colorize())
-  })
-)
 
 const stream = {
   write: (message: string) => {
